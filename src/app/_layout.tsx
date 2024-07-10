f5d03e9daff91c82d34f7371cade3eaae449abf1
Lucas Panizio <lucas_panizio@hotmail.com>
2024-07-10 20:46:04 -0300

    :sparkles: feat: add statusbar and load fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,31 @@
 import '@/styles/global.css';
 import { Slot } from 'expo-router';
+import { StatusBar, View } from 'react-native';
+
+import {
+  useFonts,
+  Inter_500Medium,
+  Inter_400Regular,
+  Inter_600SemiBold,
+} from '@expo-google-fonts/inter';
 
 export default function Layout() {
-  return <Slot />;
+  const [fontsLoaded] = useFonts({
+    Inter_500Medium,
+    Inter_400Regular,
+    Inter_600SemiBold,
+  });
+
+  if (!fontsLoaded) return;
+
+  return (
+    <View className="flex-1 bg-zinc-950">
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Slot />
+    </View>
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text>Hellow World!</Text>
+      <Text className="text-zinc-400">Hellow World!</Text>
     </View>
   );
 }
