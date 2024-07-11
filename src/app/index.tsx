92c3bf45bbbd27d8e1689a0b3120f611b8b8867c
Lucas Panizio <lucas_panizio@hotmail.com>
2024-07-10 21:43:02 -0300

    :sparkles: feat: add input component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,24 @@
+import { Input } from '@/components/input';
 import { View, Text, Image } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center gap-5">
+    <View className="flex-1 justify-center items-center px-5">
       <Image
         source={require('@/assets/logo.png')}
-        className="h-9"
+        className="h-8"
         resizeMode="contain"
       />
 
       <Text className="text-zinc-400 font-regular text-center text-lg mt-3">
         Convide seus amigos e agende sua{'\n'}próxima viagem
       </Text>
+
+      <View className="w-full bg-zinc-900 px-4 rounded-lg my-8 border border-zinc-800">
+        <Input>
+          <Input.Field placeholder="Para onde deseja ir ?" />
+        </Input>
+      </View>
     </View>
   );
 }
---
+++ src/components/input.tsx
@@ -0,0 +1,45 @@
+import { ReactElement } from 'react';
+import { TextInput, TextInputProps, View, Platform } from 'react-native';
+import clsx from 'clsx';
+
+import { colors } from '@/styles/colors';
+
+type Variant = 'primary' | 'secondary' | 'tertiary';
+
+type InputProps = {
+  children: ReactElement;
+  variant?: Variant;
+};
+
+function Input({ children, variant = 'primary' }: InputProps) {
+  return (
+    <View
+      className={clsx(
+        'w-full h-16 flex-row items-center gap-2',
+        {
+          'h-14 px-4 rounded-lg border border-zinc-800': variant !== 'primary',
+        },
+        { 'bg-zinc-950': variant === 'secondary' },
+        { 'bg-zinc-900': variant === 'tertiary' },
+      )}
+    >
+      {children}
+    </View>
+  );
+}
+
+function Field({ ...props }: TextInputProps) {
+  return (  
+    <TextInput
+      className="flex-1 text-zinc-100 text-lg font-regular"
+      placeholderTextColor={colors.zinc[400]}
+      cursorColor={colors.zinc[100]}
+      selectionColor={Platform.OS === 'ios' ? colors.zinc[100] : undefined}
+      {...props}
+    />
+  );
+}
+
+Input.Field = Field;
+
+export { Input };
