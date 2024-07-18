ca5a172cde7f277e62a4e875bb37c6da965b2744
Lucas Panizio <lucas_panizio@hotmail.com>
2024-07-18 00:49:39 -0300

    :sparkles: feat: upd input comptor show error below

--- src/components/inputs/text.tsx
+++ src/components/inputs/text.tsx
@@ -1,4 +1,11 @@
-import { ReactNode, useMemo } from 'react';
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 import { TextInput, TextInputProps, View, Platform, Text } from 'react-native';
 import { colors } from '@/styles/colors';
 import clsx from 'clsx';
@@ -9,6 +16,11 @@ import {
   UseFormReturn,
 } from 'react-hook-form';
 
+const ErrorContext = createContext<{
+  error: string | undefined;
+  setError: (error: string | undefined) => void;
+}>({ error: undefined, setError: () => {} });
+
 export type Variant = 'primary' | 'secondary' | 'tertiary';
 
 type InputProps = {
@@ -18,20 +30,24 @@ type InputProps = {
 };
 
 function Input({ children, className, variant = 'primary' }: InputProps) {
+  const [error, setError] = useState<string | undefined>();
+
   return (
-    <View
-      className={clsx(
-        'min-h-16 max-h-16 flex-row items-center gap-2',
-        {
-          'h-14 px-4 rounded-lg border border-zinc-800': variant !== 'primary',
-          'bg-zinc-950': variant === 'secondary',
-          'bg-zinc-900': variant === 'tertiary',
-        },
-        className,
-      )}
-    >
-      {children}
-    </View>
+    <ErrorContext.Provider value={{ error, setError }}>
+      <View className={className}>
+        <View
+          className={clsx('min-h-16 max-h-16 flex-row items-center gap-2', {
+            'h-14 px-4 rounded-lg border border-zinc-800':
+              variant !== 'primary',
+            'bg-zinc-950': variant === 'secondary',
+            'bg-zinc-900': variant === 'tertiary',
+          })}
+        >
+          {children}
+        </View>
+        {error && <Text className="px-2 text-rose-600">{error}</Text>}
+      </View>
+    </ErrorContext.Provider>
   );
 }
 
@@ -49,36 +65,33 @@ function Field<T extends FieldValues>({
   ...props
 }: IFieldProps<T>) {
   const { control, formState, getFieldState } = formRef;
+  const { setError } = useContext(ErrorContext);
 
-  const { error } = useMemo(() => {
-    return getFieldState(name);
-  }, [formState.errors]);
+  useEffect(() => {
+    const { error } = getFieldState(name);
+    setError(error?.message);
+  }, [formState.errors, getFieldState, name, setError]);
 
   return (
-    <>
-      <Controller
-        control={control}
-        name={name}
-        render={({ field: { onChange, onBlur } }) => (
-          <TextInput
-            className="flex-1 text-zinc-100 text-lg font-regular"
-            placeholderTextColor={colors.zinc[400]}
-            cursorColor={colors.zinc[100]}
-            maxLength={maxLength}
-            selectionColor={
-              Platform.OS === 'ios' ? colors.zinc[100] : undefined
-            }
-            onBlur={onBlur}
-            onChangeText={(text) => {
-              onChange({ target: { value: text } });
-              if (onChangeText) onChangeText(text);
-            }}
-            {...props}
-          />
-        )}
-      />
-      {error && <Text>{error.message}</Text>}
-    </>
+    <Controller
+      control={control}
+      name={name}
+      render={({ field: { onChange, onBlur } }) => (
+        <TextInput
+          className="flex-1 text-zinc-100 text-lg font-regular"
+          placeholderTextColor={colors.zinc[400]}
+          cursorColor={colors.zinc[100]}
+          maxLength={maxLength}
+          selectionColor={Platform.OS === 'ios' ? colors.zinc[100] : undefined}
+          onBlur={onBlur}
+          onChangeText={(text) => {
+            onChange({ target: { value: text } });
+            if (onChangeText) onChangeText(text);
+          }}
+          {...props}
+        />
+      )}
+    />
   );
 }
 
