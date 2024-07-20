a24305aa0ea3912703d890ae9e27418448215291
Lucas Panizio <lucas_panizio@hotmail.com>
2024-07-20 15:59:53 -0300

    :sparkles: feat: updated tab details
    > List of participants
    > Modal to create link

--- src/app/trip/details.tsx
+++ src/app/trip/details.tsx
@@ -4,15 +4,16 @@ import { useForm } from 'react-hook-form';
 import { PlusIcon } from 'lucide-react-native';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useMutation, useQuery } from '@tanstack/react-query';
-import { validateInput } from '@/utils/validateInput';
 import { linksServer } from '@/services/api/links';
+import { participantsServer } from '@/services/api/participants';
+import { DetailsForm } from '@/types/forms';
+import { schema } from '@/types/schemas';
 import { colors } from '@/styles/colors';
 import { Button } from '@/components/button';
 import { Input } from '@/components/inputs/text';
 import { Modal } from '@/components/modal';
-import { schema } from '@/types/schemas';
-import { DetailsForm } from '@/types/forms';
 import { TripLink } from '@/components/tripLink';
+import { Participant } from '@/components/participant';
 
 interface IDetailsProps {
   tripId: string;
@@ -25,15 +26,20 @@ export function Details({ tripId }: IDetailsProps) {
   });
   const [showModal, setShowModal] = useState(false);
 
-  const {
-    data: linksData = [],
-    isLoading,
-    refetch,
-  } = useQuery({
+  const { data: linksData = [], refetch: refetchLinks } = useQuery({
     queryKey: ['get_links', tripId],
     queryFn: () =>
       linksServer.getLinksByTripId(tripId).then(({ data }) => data.links),
   });
+
+  const { data: participantsData = [], refetch: refetchParticipants } =
+    useQuery({
+      queryKey: ['get_participants', tripId],
+      queryFn: () =>
+        participantsServer
+          .getByTripId(tripId)
+          .then((data) => data.participants),
+    });
 
   const { mutate: create, isPending: isCreatingLink } = useMutation({
     mutationKey: ['create_link'],
@@ -49,7 +55,7 @@ export function Details({ tripId }: IDetailsProps) {
     const body = { tripId, ...formData };
     create(body, {
       onSuccess: () => {
-        refetch();
+        refetchLinks();
         handleCloseModal();
         Alert.alert('', 'Link cadastrado com sucesso!');
       },
@@ -61,11 +67,11 @@ export function Details({ tripId }: IDetailsProps) {
 
   return (
     <View className="flex-1 mt-6">
-      <Text className="text-zinc-50 text-2xl font-semibold mb-4">
-        Links importantes
-      </Text>
-
       <View>
+        <Text className="text-zinc-50 text-2xl font-semibold mb-4">
+          Links importantes
+        </Text>
+
         {linksData.length > 0 ? (
           <FlatList
             data={linksData}
@@ -76,51 +82,65 @@ export function Details({ tripId }: IDetailsProps) {
           />
         ) : (
           <Text className="text-zinc-400 font-regular text-base mt-2 mb-6">
-            Nenhum texto adicionado.
+            Nenhum link adicionado.
           </Text>
         )}
         <Button variant="secondary" onPress={() => setShowModal(true)}>
           <PlusIcon color={colors.zinc[200]} size={20} />
           <Button.Title>Cadastrar novo link</Button.Title>
         </Button>
+
+        <Modal
+          title="Cadastrar link"
+          subtitle="Todos os convidados podem visualizar os links importantes."
+          visible={showModal}
+          onClose={handleCloseModal}
+        >
+          <View className="gap-2 mb-3">
+            <Input variant="secondary">
+              <Input.Field formRef={form} name="title" placeholder="Título" />
+            </Input>
+
+            <Input variant="secondary">
+              <Input.Field
+                formRef={form}
+                name="url"
+                placeholder="URL"
+                keyboardType="url"
+                autoCapitalize="none"
+                autoCorrect={false}
+              />
+            </Input>
+          </View>
+
+          <Button
+            onPress={form.handleSubmit(handleCreateLink)}
+            isLoading={isCreatingLink}
+          >
+            <Button.Title>Salvar link</Button.Title>
+          </Button>
+        </Modal>
       </View>
 
       <View className="flex-1 border-t border-zinc-800 mt-6">
         <Text className="text-zinc-50 text-2xl font-semibold my-6">
           Convidados
         </Text>
+
+        {participantsData.length > 0 ? (
+          <FlatList
+            data={participantsData}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <Participant data={item} />}
+            contentContainerClassName="gap-4"
+            className="mb-4"
+          />
+        ) : (
+          <Text className="text-zinc-400 font-regular text-base mt-2 mb-6">
+            Nenhum convidado adicionado.
+          </Text>
+        )}
       </View>
-
-      <Modal
-        title="Cadastrar link"
-        subtitle="Todos os convidados podem visualizar os links importantes."
-        visible={showModal}
-        onClose={handleCloseModal}
-      >
-        <View className="gap-2 mb-3">
-          <Input variant="secondary">
-            <Input.Field formRef={form} name="title" placeholder="Título" />
-          </Input>
-
-          <Input variant="secondary">
-            <Input.Field
-              formRef={form}
-              name="url"
-              placeholder="URL"
-              keyboardType="url"
-              autoCapitalize="none"
-              autoCorrect={false}
-            />
-          </Input>
-        </View>
-
-        <Button
-          onPress={form.handleSubmit(handleCreateLink)}
-          isLoading={isCreatingLink}
-        >
-          <Button.Title>Salvar link</Button.Title>
-        </Button>
-      </Modal>
     </View>
   );
 }
