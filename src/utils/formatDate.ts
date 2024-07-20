2c861ae522dbc831a6512ce0c8563ede7fc2a583
Lucas Panizio <lucas_panizio@hotmail.com>
2024-07-20 15:53:02 -0300

    :wrench: feat(utils): add new fn to format range of dates

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import { calendarUtils } from './calendarUtils';
 
 /**
  * Formata uma data no formato 'YYYY-MM-DD' para 'DD de MMM.'
@@ -19,16 +20,42 @@ export const isoToFullDateWithShortMonth = (date: string): string => {
 };
 
 /**
+ * Formata um intervalo de datas a partir da data inicial e final em uma string com a data por extenso incluindo o mês completo com a primeira letra maiúscula.
+ *
+ * @param startsAt - Uma string representando a data inicial, no formato 'YYYY-MM-DD'.
+ * @param endsAt - Uma string representando a data final, no formato 'YYYY-MM-DD'.
+ * @returns Uma string com o intervalo de datas formatado, onde a primeira letra do mês é maiúscula.
+ *
+ * @example
+ * // Entrada: '2024-07-16', '2024-07-19'
+ * // Saída: '16 à 19 de Julho'
+ */
+export const dateRangeWithFullMonth = (
+  startsAt: string,
+  endsAt: string,
+): string => {
+  return calendarUtils.formatDatesInText({
+    startsAt: dayjs(startsAt),
+    endsAt: dayjs(endsAt),
+  });
+};
+
+/**
  * Formata um intervalo de datas para garantir que o mês é abreviado.
  *
- * @param formattedDates - Uma string representando um intervalo de datas, como '16 à 19 de Julho'.
+ * @param startsAt - Uma string representando a data inicial, no formato 'YYYY-MM-DD'.
+ * @param endsAt - Uma string representando a data final, no formato 'YYYY-MM-DD'.
  * @returns Uma string com o intervalo de datas formatado, onde o mês é abreviado e a primeira letra é maiúscula.
  *
  * @example
- * // Entrada: '16 à 19 de Julho'
+ * // Entrada: '2024-07-16', '2024-07-19'
  * // Saída: '16 à 19 de Jul.'
  */
-export const dateRangeWithShortMonth = (formattedDates: string): string => {
+export const dateRangeWithShortMonth = (
+  startsAt: string,
+  endsAt: string,
+): string => {
+  const formattedDates = dateRangeWithFullMonth(startsAt, endsAt);
   const formattedDatesArray = formattedDates.split(' ');
   formattedDatesArray[formattedDatesArray.length - 1] = formattedDatesArray[
     formattedDatesArray.length - 1
